c85c1b5fa7cedeafdbfc0b27269595d428dd6e16
Archanalab <83276642+Archanalab@users.noreply.github.com>
2021-09-07 20:13:39 +0530

    Add files via upload

---
+++ My file.js
@@ -0,0 +1,6 @@
+function press()
+{var x=document.getElementById("name").value;
+var y=document.getElementById("em").value;
+document.getElementById("sub").setAttribute("style","display:none;");
+document.getElementById("heading").innerHTML="Subject:"+x+"To:"+y;}
+
